a7f4973d0af26e5256ab4d027a2af7369995a54f
Raymond Luo <rymdluo@users.noreply.github.com>
2017-04-06 02:15:40 +0800

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,17 @@
+javascript: (function () {
+    if (!window.location.href.includes("gfycat.com/upload")) {
+        window.location.href = 'https://gfycat.com/upload';
+    }
+    var x = document.querySelectorAll(".gfyLink");
+    x.forEach(function (element) {
+        var href = element.getAttribute("href");
+        href = "https://zippy.gfycat.com" + href + ".mp4";
+        var newElement = document.createElement("a");
+        newElement.setAttribute("href", href);
+        newElement.setAttribute("target", "_blank");
+        newElement.innerHTML = href;
+        element.parentNode.replaceChild(newElement, element);
+        newElement.style.backgroundColor = "#000000";
+        newElement.style.color = "#16e579";
+    });
+})();
